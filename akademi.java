0ff92356292c381bdfd500086ef509bded20a188
LeToyek <maulanatoyek@gmail.com>
2021-12-08 09:31:01 +0700

    nyediain function

--- akademi.java
+++ akademi.java
@@ -1,5 +1,64 @@
+import java.util.Scanner;
+
+import javax.sound.sampled.SourceDataLine;
+
 public class akademi {
+    static Scanner in;
     public static void main(String[] args) {
-        System.out.println("hello world");
+        showMenu();
+    }
+    static void showMenu(){
+        in = new Scanner(System.in);
+        int pilihan = 0;
+        while (pilihan != 7){
+            System.out.println("Silahkan pilih menu :" 
+            + "\n1. Data Mahasiswa"
+            + "\n2. Mata kuliah"
+            + "\n3. Input dan kelola nilai"
+            + "\n4. Cetak KHS/Rapor Nilai"
+            + "\n5. Nilai per mata kuliah"
+            + "\n6. Peringkat Mahasiswa"
+            + "\n7. Keluar");
+            pilihan = in.nextInt();
+            switch(pilihan){
+                case 1:
+                dataMahasiswa();
+                break;
+                case 2:
+                mataKuliah();
+                break;
+                case 3:
+                kelolaNilai();
+                break;
+                case 4:
+                cetakKHS();
+                break;
+                case 5:
+                nilaiPerMatkul();
+                break;
+                case 6:
+                peringkatMhs();
+                break;
+                case 7:
+                break;
+                default:
+                System.out.println(
+                    "pilihan anda tidak terdapat dalam menu"
+                );
+
+            }
+        }
+    }
+    static void peringkatMhs() {
+    }
+    static void nilaiPerMatkul() {
+    }
+    static void cetakKHS() {
+    }
+    static void kelolaNilai() {
+    }
+    static void mataKuliah() {
+    }
+    static void dataMahasiswa() {
     }
 }
